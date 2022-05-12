e67db190659560b065b164dccea6b1c01e2bcf6b
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2022-05-12 12:06:53 +0200

    updated first version of the app

--- api/decompose_roman.py
+++ api/decompose_roman.py
@@ -1,3 +1,35 @@
 """
-
+Preprocessing utility functions that are the core backend of the APIs.
 """
+import music21
+from music21 import pitch, note, chord, roman
+
+
+def analyse_roman(roman_chord: str) -> dict:
+    """
+    Utility function that analyses a roman chord by extracting
+    its features, using the library music21
+    :param roman_chord: a roman chord formatted as a string
+    :return: a dictionary containing the chord features.
+    """
+    try:
+        chord_object = roman.RomanNumeral(roman_chord)
+    except music21.Music21Exception:
+        raise ValueError('The chord given is not a valid Roman Chord.')
+    else:
+        root = str(chord_object.root())
+        bass = str(chord_object.bass())
+        quality = chord_object.quality
+        inversion = chord_object.inversion()
+        degree = chord_object.scaleDegree
+        return {'chord': roman_chord,
+                'quality': quality,
+                'inversion': inversion,
+                'degree': degree,
+                'root': root,
+                'bass': bass}
+
+
+if __name__ == '__main__':
+    abc = analyse_roman('VII64/ii')
+    print(abc)
--- api/main.py
+++ api/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
+from api.decompose_roman import analyse_roman
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"error": "No chord given"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/roman-chord-ontology/{roman_chord}")
+async def say_hello(roman_chord: str) -> dict:
+    return analyse_roman(roman_chord)
